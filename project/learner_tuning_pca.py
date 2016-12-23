965dc6ee7cdae26bccfa962f1a1892ca89e5accb
Akond Rahman <akond.rahman.buet@gmail.com>
2016-12-22 21:09:02 -0500

    pca stuff

---
+++ project/learner_tuning_pca.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Nov 21, 2016
+
+"""
+
+
+
+
+import sys, numpy as np
+import param_exp_classifier , IO_ , param_exp_analysis, pca_mobilesoft
+
+def createMobileSoftFeatures(allFeatureParam, selectedIndicies):
+  feature_dataset_to_ret = allFeatureParam.iloc[:, selectedIndicies]
+  return feature_dataset_to_ret
+
+
+def mobilesoft_random_forest(fileNameParam, fileToWriteP):
+
+  testAndTrainData = IO_.giveTestAndTrainingData(fileNameParam)
+  trainData = testAndTrainData[0]
+  testData  = testAndTrainData[1]
+  #print trainData
+  selected_training_data = pca_mobilesoft.getPCAedFeatures(trainData)
+  print "Size of selected training data : ", np.shape(selected_training_data)
+  print "="*50
+
+  dict_of_results = param_exp_classifier.runRandomForest(selected_training_data, testData)
+  reportStr = param_exp_analysis.analyzeThis(dict_of_results)
+  IO_.writeStrToFile(fileToWriteP, reportStr)
+
+def mobilesoft_svm(fileNameParam, fileToWriteP):
+
+  testAndTrainData = IO_.giveTestAndTrainingData(fileNameParam)
+  trainData = testAndTrainData[0]
+  testData  = testAndTrainData[1]
+  selected_training_data = pca_mobilesoft.getPCAedFeatures(trainData)
+  print "Size of selected training data : ", np.shape(selected_training_data)
+  print "="*50
+
+  dict_of_results = param_exp_classifier.runSVM(selected_training_data, testData, 0.90)
+  reportStr = param_exp_analysis.analyzeThis(dict_of_results)
+  IO_.writeStrToFile(fileToWriteP, reportStr)
+def mobilesoft_knn(fileNameParam, fileToWriteP):
+
+  testAndTrainData = IO_.giveTestAndTrainingData(fileNameParam)
+  trainData = testAndTrainData[0]
+  testData  = testAndTrainData[1]
+  selected_training_data = pca_mobilesoft.getPCAedFeatures(trainData)
+  print "Size of selected training data : ", np.shape(selected_training_data)
+  print "="*50
+
+  dict_of_results = param_exp_classifier.runKNN(selected_training_data, testData, 0.90)
+  reportStr = param_exp_analysis.analyzeThis(dict_of_results)
+  IO_.writeStrToFile(fileToWriteP, reportStr)
+def mobilesoft_cart(fileNameParam, fileToWriteP):
+  testAndTrainData = IO_.giveTestAndTrainingData(fileNameParam)
+  trainData = testAndTrainData[0]
+  testData  = testAndTrainData[1]
+  selected_training_data = pca_mobilesoft.getPCAedFeatures(trainData)
+  print "Size of selected training data : ", np.shape(selected_training_data)
+  print "="*50
+
+  dict_of_results = param_exp_classifier.runCART(selected_training_data, testData, 0.90)
+  reportStr = param_exp_analysis.analyzeThis(dict_of_results)
+  IO_.writeStrToFile(fileToWriteP, reportStr)
+print "Started at: ", IO_.giveTimeStamp()
+fileNameParam="Exp_1_Mobilesoft_clusterified_1407.csv"
+fileToWrite="param_exp_combo_mobilesoft_ten_folds.csv"
+mobilesoft_random_forest(fileNameParam, 'rf_'+fileToWrite)
+#mobilesoft_svm(fileNameParam, 'svm_'+fileToWrite)
+#mobilesoft_knn(fileNameParam, 'knn_'+fileToWrite)
+#mobilesoft_cart(fileNameParam, 'cart_'+fileToWrite)
+print "Ended at: ", IO_.giveTimeStamp()
---
+++ project/pca_mobilesoft.py
@@ -0,0 +1,80 @@
+'''
+Akond, Dec 22, 2016
+'''
+
+from sklearn import decomposition
+import IO_, sys, exp_x_classifiers
+import numpy as np
+def getPCAedFeatures(all_features):
+    '''
+    PCA reff:
+    1. http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html#sklearn.decomposition.PCA.fit
+    2. http://scikit-learn.org/dev/tutorial/statistical_inference/unsupervised_learning.html#principal-component-analysis-pca
+    '''
+    pcaObj = decomposition.PCA(n_components=21)
+    pcaObj.fit(all_features)
+    # variance of features
+    variance_of_features = pcaObj.explained_variance_
+    # how much variance is explained each component
+    variance_ratio_of_features = pcaObj.explained_variance_ratio_
+    print "Explained varaince ratio"
+    for index_ in xrange(len(variance_ratio_of_features)):
+        print "Principal component#{}, explained variance:{}".format(index_+1, variance_ratio_of_features[index_])
+    #print variance_ratio_of_features
+    print "-"*50
+    selective_feature_indices = [x_ for x_ in variance_of_features if x_ > float(1) ]
+    no_features_to_use = len(selective_feature_indices)
+    # see how much explained variance is covered by the number of compoenents , and set the number
+    no_features_to_use = 1 #using one PCA we get >95% variance
+    print "Of all the features, we will use:", no_features_to_use
+    print "-"*50
+    pcaObj.n_components=no_features_to_use
+    selected_features = pcaObj.fit_transform(all_features)
+    print "Selected feature dataset size:", np.shape(selected_features)
+    print "-"*50
+    return selected_features
+
+
+def experiment_mobilesoft_random_forest(fileNameParam):
+  testAndTrainData = IO_.giveTestAndTrainingData(fileNameParam)
+  trainData = testAndTrainData[0]
+  testData  = testAndTrainData[1]
+  #print trainData
+  selected_training_data = getPCAedFeatures(trainData)
+  print "Size of selected training data : ", np.shape(selected_training_data)
+  print "="*50
+  exp_x_classifiers.runRandomForest(selected_training_data, testData, 0.90)
+  print "="*50
+
+def experiment_mobilesoft_cart(fileNameParam):
+  testAndTrainData = IO_.giveTestAndTrainingData(fileNameParam)
+  trainData = testAndTrainData[0]
+  testData  = testAndTrainData[1]
+  #print trainData
+  selected_training_data = getPCAedFeatures(trainData)
+  print "Size of selected training data : ", np.shape(selected_training_data)
+  print "="*50
+  exp_x_classifiers.runCART(selected_training_data, testData, 0.90)
+  print "="*50
+
+def experiment_mobilesoft_svm(fileNameParam):
+  testAndTrainData = IO_.giveTestAndTrainingData(fileNameParam)
+  trainData = testAndTrainData[0]
+  testData  = testAndTrainData[1]
+  #print trainData
+  selected_training_data = getPCAedFeatures(trainData)
+  print "Size of selected training data : ", np.shape(selected_training_data)
+  print "="*50
+  exp_x_classifiers.runSVM(selected_training_data, testData, 0.90)
+  print "="*50
+
+def experiment_mobilesoft_knn(fileNameParam):
+  testAndTrainData = IO_.giveTestAndTrainingData(fileNameParam)
+  trainData = testAndTrainData[0]
+  testData  = testAndTrainData[1]
+  #print trainData
+  selected_training_data = getPCAedFeatures(trainData)
+  print "Size of selected training data : ", np.shape(selected_training_data)
+  print "="*50
+  exp_x_classifiers.runKNN(selected_training_data, testData, 0.90)
+  print "="*50
