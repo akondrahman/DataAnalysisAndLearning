d4d8b31cf8ff086c64e7c58326fdbb77f43661ed
Akond Rahman <akond.rahman.buet@gmail.com>
2016-12-24 00:35:18 -0500

    pca stuff

--- project/learner_pca_main.py
+++ project/learner_pca_main.py
@@ -3,8 +3,8 @@ Akond, Dec 22, 2016
 main file to call the pca+learners
 '''
 import pca_mobilesoft, IO_, sys
-f_ = fileNameParam="Exp_1_Mobilesoft_clusterified_1407.csv"
-#pca_mobilesoft.experiment_mobilesoft_cart(f_)
+f_ ="Exp_1_Mobilesoft_clusterified_1407.csv"
+pca_mobilesoft.experiment_mobilesoft_cart(f_)
 #pca_mobilesoft.experiment_mobilesoft_knn(f_)
 #pca_mobilesoft.experiment_mobilesoft_random_forest(f_)
 #pca_mobilesoft.experiment_mobilesoft_svm(f_)
